7834dc095fb9509a3106c89e78a852e9c53b7c4a
Cloud7z <yongchangsitu@gmail.com>
2025-02-04 17:05:11 -0500

    heyy

--- ShootHouse/Assets/Scenes/YongScene/playerControl.cs
+++ ShootHouse/Assets/Scenes/YongScene/playerControl.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class playerControl : MonoBehaviour
 {
+    [SerializeField] int jump;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

94520310dcc8aea8b703ab3ba264f262d63ebc38
xXLEGITM1N3Xx <raymondrosario91@yahoo.com>
2025-02-06 16:24:54 -0500

    GameManager Update

--- ShootHouse/Assets/TutorialInfo/Scripts/Editor/GameManager.cs
+++ ShootHouse/Assets/TutorialInfo/Scripts/Editor/GameManager.cs
@@ -1,16 +1,74 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class gamemanager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static gamemanager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public bool isPaused;
+
+    public GameObject player;
+    public PlayerController playerScript;
+
+    int goalCount;
+    void Awake()
     {
-        
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePaused();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpaused();
+            }
+        }
+    }
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        goalCount += amount;
+
+        if (goalCount == 0)
+        {
+            statePaused();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
     }
 }

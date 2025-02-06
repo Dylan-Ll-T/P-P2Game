763cac76c8cf8ae939de74fcd46a22165c7c8b71
Cloud7z <yongchangsitu@gmail.com>
2025-02-05 21:43:59 -0500

    player control

--- ShootHouse/Assets/Scenes/YongScene/playerControl.cs
+++ ShootHouse/Assets/Scenes/YongScene/playerControl.cs
@@ -1,18 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class playerControl : MonoBehaviour
+public class playerController : MonoBehaviour
 {
-    [SerializeField] int jump;
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpMax;
+    [SerializeField] int gravity;
+
+
+    int jumpCount;
+
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+    bool isSprinting;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+        //moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //transform.position += moveDir * speed * Time.deltaTime;
+
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) +
+                  (Input.GetAxis("Vertical") * transform.forward);
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+
+        controller.Move(playerVel * Time.deltaTime);
+        playerVel.y -= gravity * Time.deltaTime;
+       
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
     }
 }

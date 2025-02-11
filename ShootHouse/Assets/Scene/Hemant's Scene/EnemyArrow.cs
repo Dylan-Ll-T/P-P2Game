6b3315bcb540d91dc33bb6b2599375179c6d9282
HEMZU2006 <hemantrs2006@gmail.com>
2025-02-11 15:46:59 -0500

    enemy indicator

---
+++ ShootHouse/Assets/Scene/Hemant's Scene/EnemyArrow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyArrow : MonoBehaviour
+{
+    public Transform Player;
+    public Transform Enemy;
+    public CanvasGroup IsVisable;
+    public float distance;
+    public Image AllertArrow;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(Player.transform.position, Enemy.transform.position);
+        if (distance <= 50)
+        {
+            IsVisable.alpha = 1;
+            if (distance > 15)
+            {
+                AllertArrow.color = Color.cyan;
+            }
+            else
+            {
+                AllertArrow.color = Color.yellow;
+            }
+        }
+        else
+        {
+            IsVisable.alpha = 0;
+
+        }
+    }
+}
